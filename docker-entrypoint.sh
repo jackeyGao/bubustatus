dc4f4bb324be4ee76240fb9c507d613ffe25f7b6
jackeyGao <gaojunqi@outlook.com>
2015-09-25 14:02:14 +0800

    增加创建管理员脚本

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,4 +5,11 @@
 # Created Time: 五  9/25 10:31:45 2015
 
 /code/manage.py syncdb --noinput
+
+echo "from django.contrib.auth.models import User
+if not User.objects.filter(username='admin').count():
+    User.objects.create_superuser('admin', 'admin@example.com', 'pass')
+" | python manage.py shell
+
+
 /usr/local/bin/gunicorn bubu.wsgi:application -w 2 -b :8000
