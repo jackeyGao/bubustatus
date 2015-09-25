1e21ca3e140095d4472ecb66360f8acc11989532
jackeyGao <gaojunqi@outlook.com>
2015-09-25 11:29:29 +0800

    Add docker support

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,4 +5,4 @@
 # Created Time: 五  9/25 10:31:45 2015
 
 /code/manage.py syncdb --noinput
-/usr/local/bin/gunicorn config.wsgi:application -w 2 -b :8000
+/usr/local/bin/gunicorn bubu.wsgi:application -w 2 -b :8000
